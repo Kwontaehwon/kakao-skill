0aa5618d1b7f86b106d5473d04ffb5f372b16b31
권태현 <kygkth2011@gmail.com>
2023-06-09 06:02:31 +0900

    feat : 인자를 받아 문서 생성

--- docx_generator.py
+++ docx_generator.py
@@ -1,19 +1,23 @@
+import sys
 from docx import Document
 
-deposit = 1000000 
-end_date = "2023년 12월 31일"
-home_address = "주소 입력"
+def generate_docx(deposit, end_date, home_address) : 
+    # 문서 파일 로드
+    document = Document("template.docx") 
 
-# 문서 파일 로드
-document = Document("template.docx") 
+    # 변수 값을 문서에 적용
+    for paragraph in document.paragraphs:
+        if "[deposit]" in paragraph.text:
+            paragraph.text = paragraph.text.replace("[deposit]", str(deposit))
+        if "[end_date]" in paragraph.text:
+            paragraph.text = paragraph.text.replace("[end_date]", end_date)
+        if "[home_address]" in paragraph.text:
+            paragraph.text = paragraph.text.replace("[home_address]", home_address)
 
-# 변수 값을 문서에 적용
-for paragraph in document.paragraphs:
-    if "[deposit]" in paragraph.text:
-        paragraph.text = paragraph.text.replace("[deposit]", str(deposit))
-    if "[end_date]" in paragraph.text:
-        paragraph.text = paragraph.text.replace("[end_date]", end_date)
-    if "[home_address]" in paragraph.text:
-        paragraph.text = paragraph.text.replace("[home_address]", home_address)
+    document.save("output.docx")
 
-document.save("output.docx")
+    print("GENERATED DONE") 
+
+
+if __name__ == '__main__':
+    generate_docx(sys.argv[1], sys.argv[2], sys.argv[3])
